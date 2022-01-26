4b4618228e20eb094fbd02409b30d0272aadfa8b
Romain GAUVREAU <romain.gauvreau@epita.fr>
2022-01-26 12:29:05 +0100

    Fix Client Launcher

--- client/src/main/java/fr/lernejo/chat/Launcher.java
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -1,8 +1,8 @@
 package fr.lernejo.chat;
 
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Scanner;
 
@@ -10,8 +10,9 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
 
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(Launcher.class);
+        RabbitTemplate rabbitTemplate = appContext.getBean(RabbitTemplate.class);
+        rabbitTemplate.setRoutingKey("chat_messages");
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
@@ -20,6 +21,7 @@ public class Launcher {
             System.out.println("Input a message, we will sent it for you (q for quit)");
             userInput = scanner.nextLine();
             if (userInput.equals("q")) {
+                System.out.println("Bye");
                 System.exit(0);
             } else {
                 rabbitTemplate.convertAndSend("chat_messages", userInput);
