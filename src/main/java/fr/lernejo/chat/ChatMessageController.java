ea4817db93117fd2353856325bf58a9bd02caa6a
Romain GAUVREAU <romain.gauvreau@epita.fr>
2022-01-26 11:19:14 +0100

    Consultation des 10 derniers messages

---
+++ src/main/java/fr/lernejo/chat/ChatMessageController.java
@@ -0,0 +1,21 @@
+package fr.lernejo.chat;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ChatMessageController {
+
+    @GetMapping(value = "/message")
+    public ResponseEntity<List<String>> getAll() {
+        return new ResponseEntity<>(ChatMessageRepository.getLastTenMessages(), HttpStatus.OK);
+    }
+
+}
--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class ChatMessageListener {
 
     public void onMessage(String message) {
-        System.out.println(message);
+        ChatMessageRepository.addChatMessage(message);
     }
 }
